61b8ed871d6b030e6779ff24dbbcf9f0016c3c9b
loriful <tiptonica@gmail.com>
2022-05-11 01:33:55 -0400

    inits done, styling done

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,58 +27,123 @@ var quizArray = [
     }
 ];
 
-var timer = (quizArray.length * 10) + 10;             // Initial clock time, 10 seconds per question, and 10 extra
-var timerEl = document.getElementById('clock');
-
 /////////////////////////////////////////////////////////////////////////////////////////
-function storeUser () {
+function storePlayer (inits, timer) {
 
-    console.log("storeUser function");
+    event.preventDefault();
 
-};  // end function storeUser
+    var playerArray = [playerObj = {
+                inits: inits,
+                score: timer,
+        }];
+
+    if (!localStorage.getItem("savedScores")) {         // no scores stored locally
+        localStorage.setItem("savedScores", JSON.stringify(playerArray));
+    } else {
+        var savedStorage = JSON.parse(localStorage.getItem("savedScores"));
+        savedStorage.push(playerArray[0]);
+        localStorage.setItem("savedScores", JSON.stringify(savedStorage));
+    };
+
+    displayScores();
+
+};  // end function storePlayer
 
 function displayScores () {
-    console.log("in displayScores");
+
+    var handle = document.querySelector("#quiz-next");
+
+    if (handle) {
+        document.querySelector("#quiz-next").remove();
+    };
+
+    document.querySelector("#header").style.display = "none";             // clear scores link and time
+
+    var lastQuiz = document.createElement("div");    // get a new container for last screen
+    lastQuiz.id = "quiz-next";                            
+    lastQuiz.innerHTML = "<h1>High Scores</h1><ul class=finalscores>";
+    
+    var scores = JSON.parse(localStorage.getItem("savedScores"));
+
+    if (!scores) {              // no scores in storage
+        scores = " ";
+    } else {
+        // loop through scores array
+        for (var i = 0; i < scores.length; i++) {  
+            var scoreEl = document.createElement("li");  
+            scoreEl.innerHTML = scores[i].inits + ": " + scores[i].score;
+            lastQuiz.append(scoreEl);
+            };
+        };
+
+    var addButtons = document.createElement("div");
+
+    addButtons.innerHTML = "<button class=last-btn id=goback-btn type=button>Go Back</button>" +
+        "<button class=last-btn id=clear-btn type=button>Clear high scores</button>";
+    lastQuiz.append(addButtons); 
+
+    document.getElementById("quiz-box").appendChild(lastQuiz);      //display scores
+
+    
+    // listen for next step either button
+
+    document.getElementById("goback-btn").addEventListener("click", () => {
+        document.querySelector("#quiz-next").remove();           // clear last quiz screen
+        document.querySelector("#header").style.display = "flex";       // put header back
+        initQuiz();                                                     // start new quiz
+    }); 
+
+    document.getElementById("clear-btn").addEventListener("click", () => {
+        localStorage.clear();
+        displayScores();
+    }); 
+
 };  // end function displayScores
 
-function endQuiz () {
+function endQuiz(timer) {
 
     document.querySelector("#quiz-next").remove();          // clear last quiz screen
-
-    if (timer > 0) {                                    // user has points
-        storeUser();    
-    }
-    else {
-        displayScores();
-    }
-    var lastQuiz = document.createElement("div");       // get a new container for last screen
-    lastQuiz.id = "quiz-next";                           
+    
+    displayTime(timer);
+    
+    if (timer < 1) {   
+        displayScores();                                 // user has no points
+    } else {                                             // user has points, store inits
+        var lastQuiz = document.createElement("div");    // get a new container for last screen
+        lastQuiz.id = "quiz-next";                           
                                                         // build the screen elements
-    lastQuiz.innerHTML = "<h1>All Done!</h1>" + 
-    "<p>Your final score is: " + displayScores() + "<br><br>Enter your initials:</p>"
-      + "<div class=btn-box><button class='submit-btn' id='submit-btn' type='submit'>Submit</button></div>";   
+        lastQuiz.innerHTML = "<h1>All Done!</h1>" + 
+        "<h2>Your final score is: " + timer + "</h2>" +
+        "<h3 id=enter>Enter your initials:</h3>" +
+        "<form><label for=initials></label>" + 
+        "<input type=text id=initials placeholder='Your initials'/>" + 
+        "<button class='submit-btn' id='submit-btn' type='submit'>Submit</button></form>";
+   
      
     document.getElementById("quiz-box").appendChild(lastQuiz);   // display 
+    
+    // listen for intials
+    document.getElementById("submit-btn").addEventListener("click", () => {
+        if (initials.value) {
+            storePlayer(initials.value, timer);              // start button // listen for submit
+        } else {
+            alert("Please enter initials.");
+            return endQuiz(timer);
+            };
+    });
+};
   
-    displayTime(0);
-        
 };  // end function endQuiz
 
 function displayTime (current) {
     // show the clock timer
+    var timerEl = document.getElementById('clock');
     timerEl.textContent = "Time:  " + current;
-};  // end function displayTime
+};
 
-// function clockTimer (timer) {
-    
-//     console.log("in function clockTimer. Timer = " + timer);  
-//     // count down the time
-
-// };   // end function clockTimer
-
-function buildAnswer (report, num) {
+function buildAnswer (report, num, timer) {
     // (the result, the number of current question)
-    // console.log('in buildAnswer');
+
     // build the display
     var answerEl = document.createElement("div");
     answerEl.className = "answer-box";
@@ -90,43 +155,42 @@ function buildAnswer (report, num) {
         };
 
     document.getElementById("quiz-next").appendChild(answerEl);         // show result
+    
     var intervalTimer = 1;                                  // wait for user to see result
+    
     var runWait = setInterval(function () {
         intervalTimer--;
         if (intervalTimer < 1) {                            // go to next question or end quiz
             clearInterval(runWait);
             if ((num < quizArray.length-1) && (timer > 1)) {
                 num++;
-                runQuiz(num);
+                runQuiz(num, timer);
             } else { 
-                endQuiz();
+                endQuiz(timer);
             }
         };
     }, 1000);
 
 } // end function buildAnswer
 
-function checkAnswer (pick, correct, num) {
+function checkAnswer (pick, correct, num, timer) {
     // (the answer chosen, the stored correct answer, which question)
+    
     var matched; 
-    // console.log('in CheckAnswer');
     
     if (pick === correct) {  
         matched = true;
        } else {
-        timer = timer-10;
+        timer = timer-10;           // wrong answer, loose 10 points
         matched = false;
        };
-    // console.log('match = ' + matched);
-    buildAnswer(matched, num);              // display the result for each question
-    // return selection;
+    
+    buildAnswer(matched, num, timer);              // display the result for each question
 
 } // end checkAnswer
 
-function getAnswer (correct, num) {
-
-    // console.log('in getAnswer this is correct ' + correct + 'this is num ' + num);
-
+function getAnswer (correct, num, timer) {
+    
     var intervalTimer = 10;                     // ten seconds per question
     
     if (timer < intervalTimer) {                // less than 10 seconds left
@@ -138,33 +202,24 @@ function getAnswer (correct, num) {
         displayTime(timer--);
         if (intervalTimer < 1) {                    // out of time
             clearInterval(runClock);
-            // console.log("timer has run out");
-            return checkAnswer("", correct, num);   // determine if answer correct
+            return checkAnswer("", correct, num, timer);   // determine if answer correct
         };
-        // console.log('Time Interval: ' + intervalTimer);
     }, 1000);
-    // document.querySelectorAll('.some-class').forEach(item => {
-    //     item.addEventListener('click', event => {
+   
     document.querySelectorAll("#quiz-btn").forEach(item => {        // listen on each answer button
         item.addEventListener("click", function () {  
         clearInterval(runClock);
-        // console.log('target = ' + event.target.value);
-        checkAnswer(event.target.value, correct, num);             // button clicked, check answer
+        checkAnswer(event.target.value, correct, num, timer);             // button clicked, check answer
     }, { once:true } )});
-  
+
 }; // end getAnswer
   
 function loadQuestions (num) {
-
-    // console.log('function loadQuestions');
-    
+   
     var newScreen = document.createElement("div");           // build new quiz screen
     newScreen.id = "quiz-next";                              // tag for removal later
     newScreen.innerHTML = "<h1 class=quiz-header>" + quizArray[num].questionText + "</h1>";
     
-    // var buttonBox = document.createElement("div");              // wrapper for all buttons
-    // buttonBox.id = "answer-box";
-    // build the set of possible answers
     for (i = 0; i < quizArray[num].answerArray.length; i++) {      
         var btnWrap = document.createElement("div");             //wrapper for button
         btnWrap.id = "btn-box";
@@ -172,51 +227,25 @@ function loadQuestions (num) {
         newScreen.appendChild(btnWrap);                      // add the answer to the list                      
     }  // for
 
-    // newScreen.appendChild(buttonBox);
-
     document.querySelector("#quiz-box").appendChild(newScreen);     // display the set
-    // console.log('this is correct', question.correct);
-    // console.log('about to call getAnswer, num = ' + num);
+    
     return quizArray[num].correct;                              // send the correct answer to getAnswer
-    // getAnswer(quizArray[num].correct,num);
 
 }; // end loadQuestions
 
-function runQuiz (num) {                                 
-    ////////////// REPEAT UNTIL ALL QUESTIONS DISPLAYED OR TIME RUNS OUT
-    // console.log("in runQuiz, this is num " + num);
-   
+function runQuiz (num, timer) {                                 
+    // REPEAT UNTIL ALL QUESTIONS DISPLAYED OR TIME RUNS OUT
+    
     document.querySelector("#quiz-next").remove();          // clear last quiz screen
     
-    getAnswer(loadQuestions(num), num);                     // load each set of questions
-
-    // check if done
-    // if (num === quizArray.length || timer < 0) {
-    //     console.log('quizArray.length = ' + quizArray.length);
-    //     console.log("num in if " + num);
-    //     console.log('time to call endQuiz after loadQuestions & getAnswer');  
-    //     return; 
-    // };
-
-    // if (timer < 1) {
-
-    //     if (num > (quizArray.length)) {
-    //         runQuiz(num);    
-    //         num++;
-    //         // if (num === (quizArray.length)) {
-    //         //     console.log('num ' + num);
-    //             // return;
-    //     } else {
-    //     console.log('end of quiz, show score, etc.');
-    //     console.log('this is the score ' + timer);
-    //     return;
-    //     }
-    // }; 
+    getAnswer(loadQuestions(num), num, timer);                     // load each set of questions
 };
 
 function initQuiz() {   
 
-     // load initial screen to start quiz
+    var timer = (quizArray.length * 10) + 10;             // Initial clock time, 10 seconds per question, and 10 extra
+    
+    // load initial screen to start quiz
     var quizWrapper = document.querySelector("#quiz-box");          // container for quiz screen
     var initialQuiz = document.createElement("div");                // get a new container for first screen
     
@@ -230,10 +259,11 @@ function initQuiz() {
     
     displayTime(0);
                                                     // listen for start
-    document.getElementById("start-btn").addEventListener("click", () => { runQuiz(0); });        // start button
-
+    document.getElementById("start-btn").addEventListener("click", () => { runQuiz(0, timer); });        // start button
+    document.getElementById("scores").addEventListener("click", displayScores);         // activate high scores link
+          
 };  // end function initQuiz
 
-////////////////////////////////////////////////////
+/////////////////////
 
 initQuiz();
